6e2e7e2e13692166424169307f0d0fffde5c2d75
Camilo Avendaño <garaven.camilo@gmail.com>
2025-06-02 00:16:44 -0500

    feat: add UserSession class

--- app/src/main/java/com/example/finzu/UserSession.java
+++ app/src/main/java/com/example/finzu/UserSession.java
@@ -1,4 +1,29 @@
 package com.example.finzu;
 
+import com.example.finzu.User;
+
 public class UserSession {
+    private static UserSession instance;
+    private User user;
+
+    private UserSession() {}
+
+    public static synchronized UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void clearSession() {
+        user = null;
+    }
 }
