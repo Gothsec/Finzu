8f1b530a0d55307c6786d024197c8b6cf226e8e9
Camilo Avendaño <garaven.camilo@gmail.com>
2025-06-01 22:08:01 -0500

    feat: add account class

--- app/src/main/java/com/example/finzu/Account.java
+++ app/src/main/java/com/example/finzu/Account.java
@@ -1,4 +1,32 @@
 package com.example.finzu;
 
 public class Account {
+    private int id;
+    private String name;
+    private double amount;
+    private String type;
+
+    public Account(int id, String name, double amount, String type) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    public double getAmount() { return amount; }
+
+    public String getType() { return type; }
+
+    public void setId(int id) { this.id = id; }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setAmount(double amount) { this.amount = amount; }
+
+    public void setType(String type) { this.type = type; }
 }
